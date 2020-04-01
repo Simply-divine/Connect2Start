fc503f4d90207a2409aef438bdb6d2cda11aaddd
Hardik <hardikvu0204@gmail.com>
2020-04-01 20:57:52 +0530

    articles added

--- startup_app/db/schema.rb
+++ startup_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_01_113832) do
+ActiveRecord::Schema.define(version: 2020_04_01_135145) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "investors", force: :cascade do |t|
     t.string "fname"
