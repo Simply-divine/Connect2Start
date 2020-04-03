bdbf0b0f5094242e22339961667a822362a8f2a5
Hardik <hardikvu0204@gmail.com>
2020-04-03 10:58:49 +0530

    articles controller bug fixed

--- startup_app/app/controllers/articles_controller.rb
+++ startup_app/app/controllers/articles_controller.rb
@@ -16,9 +16,10 @@ class ArticlesController < ApplicationController
     end
     
 
-    private:
+    private
         def article_params
             params.require(:article).permit(:title,:description) 
         end
+
  
 end
