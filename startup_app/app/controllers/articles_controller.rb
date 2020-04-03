a9afdfb463a53d32af97e57ec8e376ab287adb13
Hardik <hardikvu0204@gmail.com>
2020-04-03 10:39:02 +0530

    articles controller new and create method added

---
+++ startup_app/app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+          flash[:success] = "Article successfully created"
+          redirect_to articles_show(@article)
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+    
+
+    private:
+        def article_params
+            params.require(:article).permit(:title,:description) 
+        end
+ 
+end
--- startup_app/config/routes.rb
+++ startup_app/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
 
   get 'investors/signup', to: 'investors#new'
   resources :investors, except: [:new]
