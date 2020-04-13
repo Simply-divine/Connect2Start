537e961f383ca99bc064153abb9f8a5b6747ba53
Hardik <hardikvu0204@gmail.com>
2020-04-13 11:42:14 +0530

    partial and destroy method added

--- startup_app/app/controllers/articles_controller.rb
+++ startup_app/app/controllers/articles_controller.rb
@@ -37,6 +37,12 @@ class ArticlesController < ApplicationController
             render 'edit'
         end
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article successfully deleted"
+        redirect_to articles_path
+    end
     private
         def article_params
             params.require(:article).permit(:title,:description) 
