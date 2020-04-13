45f10736d782197d9bad5321324232d738b1f457
Hardik <hardikvu0204@gmail.com>
2020-04-13 12:02:21 +0530

    wrap up and remove repetitions in controller

--- startup_app/app/controllers/articles_controller.rb
+++ startup_app/app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+        before_action :set_article, only: [:edit,:update,:show,:destroy]
     def new
         @article = Article.new
     end
@@ -20,15 +20,13 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
     
     def update
-        @article = Article.find(params[:id])
+
         if @article.update(article_params)
             flash[:success] = "Article successfully updated"
             redirect_to article_path(@article)
@@ -38,12 +36,16 @@ class ArticlesController < ApplicationController
         end
     end
     def destroy
-        @article = Article.find(params[:id])
+
         @article.destroy
         flash[:notice] = "Article successfully deleted"
         redirect_to articles_path
     end
     private
+        def set_article
+            @article = Article.find(params[:id]);
+        end
+
         def article_params
             params.require(:article).permit(:title,:description) 
         end
