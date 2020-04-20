b285c7c2c93020a71080ef11466226761a924a6d
C2OO13 <smitpat2000@gmail.com>
2020-04-20 21:57:28 +0530

    Added websocket

--- startup_app/app/controllers/messages_controller.rb
+++ startup_app/app/controllers/messages_controller.rb
@@ -6,6 +6,11 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create(user: current_user, room: @room,
                                    message: params.dig(:message, :message))
+    if @message.save
+      RoomChannel.broadcast_to @room, @message
+      #ActionCable.server.Broadcast "room_channel", foo: @message.body
+    end
+
   end
 
   protected
