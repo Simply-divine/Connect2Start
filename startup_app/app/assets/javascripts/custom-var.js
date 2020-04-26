a7d422b2be123aa030926d2b13e9265a66700fbb
Hardik <hardikvu0204@gmail.com>
2020-04-26 14:57:03 +0530

    UI changed

---
+++ startup_app/app/assets/javascripts/custom-var.js
@@ -0,0 +1,3 @@
+var GOOGLE_ANALYTICS_ID = "UA-164487532-1"; // example "UA-XXXXXXXX-X"
+var CRISP_WEBSITE_ID = "42d555d2-6e33-4073-8599-6469bb2e0fb3";    // example "9x5x-25x-4x67-x065-x91xxxxx6x83"
+var FORMSPREE_EMAIL = "hardikvu0204@gmail.com";     // example bdavidxyz@gmail.com
