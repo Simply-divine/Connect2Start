61460e7f19849a898f516f5dc864adf9d1439398
Hardik <hardikvu0204@gmail.com>
2020-04-26 16:19:17 +0530

    styling

--- startup_app/app/assets/javascripts/application.js
+++ startup_app/app/assets/javascripts/application.js
@@ -31,6 +31,7 @@ submit_message = function() {
         };
     });
 };
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {
--- startup_app/app/models/article.rb
+++ startup_app/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true, length: {minimum:3, maximum:30}
+    validates :title, presence: true, length: {minimum:3, maximum:100}
     validates :description, presence: true, length: {minimum:5, maximum: 1300}
     validates :user_id,presence: true
     has_many :article_categories
